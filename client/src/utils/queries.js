8b494e01361a13d2f62e4ad872e0ff1a39cb1460
ellysaschottler <ellysastork@gmail.com>
2024-02-16 07:03:34 -0600

    updated client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,60 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
-  {
-    user {
-      userName
-      Lists {
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      locations {
+        _id
+        locationtText
+        createdAt
       }
     }
   }
-  `;
+`;
+
+export const QUERY_Locations = gql`
+  query getLocations {
+    locations {
+      _id
+      locationText
+      locationAuthor
+      createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_LOCATION = gql`
+  query getSingleLocation($locationId: ID!) {
+    location(locationId: $locationId) {
+      _id
+      locationText
+      locationAuthor
+      createdAt
+      ideas {
+        _id
+        ideaText
+        ideaAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      locations {
+        _id
+        locationText
+        locationAuthor
+        createdAt
+      }
+    }
+  }
+`;
