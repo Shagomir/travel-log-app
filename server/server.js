8b4d79ba78164a049ef87f48eb48ac99d6da6d7f
ellysaschottler <ellysastork@gmail.com>
2024-02-20 20:22:51 -0600

    commented out part of server code

--- server/server.js
+++ server/server.js
@@ -31,13 +31,13 @@ const startApolloServer = async () => {
     })
   );
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
-  }
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  //   });
+  // }
 
   db.once("open", () => {
     app.listen(PORT, () => {
