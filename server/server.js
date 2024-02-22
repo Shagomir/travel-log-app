ed556d98b158d58ffbb0fd5f7e21b5325ecd03c8
ellysaschottler <ellysastork@gmail.com>
2024-02-22 11:35:22 -0600

    fixing server.js file

--- server/server.js
+++ server/server.js
@@ -31,13 +31,12 @@ const startApolloServer = async () => {
     })
   );
 
-  // if (process.env.NODE_ENV === "production") {
-  //   app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  //   app.get("*", (req, res) => {
-  //     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  //   });
-  // }
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
 
   db.once("open", () => {
     app.listen(PORT, () => {
